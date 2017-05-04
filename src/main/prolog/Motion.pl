1b5c99c0115b08860b9f6fdc851ed7c744415f66
adrianarmaselu <adi.armaselu@gmail.com>
2017-05-03 20:54:21 -0500

    Fixed Solver Engine

--- src/main/prolog/Motion.pl
+++ src/main/prolog/Motion.pl
@@ -1,41 +1,111 @@
-#include('Utils.pl')
-#include('Builtins.pl')
+#include('Utils.pl').
+#include('Builtins.pl').
 
-%TODO: THE NAMING CONVENTIONS CAN BE IMPROVED
+_solve(Goals, Params, SolutionSteps) :-
+    _solve(Goals, Params, _, [], SolutionSteps).
 
-solve(Problem, Solution) :-
-    solve(Problem, AccumulatedFacts, [], Solution).
+%Base Case
+_solve([], SolutionParams, SolutionParams, SolutionSteps, SolutionSteps).
 
-solve(problem(_, []), []).
-
-solve(
-problem(Facts, [Question | RemainingQuestions]),
-[AccumulatedFacts | AllAccumulatedFacts],
-InCalculationSequence,
-[OutCalculationSequence | FinalCalculationSequence],
-Solution
-) :-
-	solve(problem(Facts, Question), AccumulatedFacts, InCalculationSequence, OutCalculationSequence, Solution),
-	solve(problem(AccumulatedFacts, RemainingQuestions), OutCalculationSequence, FinalCalculationSequence, AllAccumulatedFacts).
+%Solve each individual problem at a time recursively
+_solve([Goal | RemainingGoals], Params, SolutionParams, Steps, SolutionSteps) :-
+	_solve_subproblem(Goal, Params, ParamsWithGoalParams, Steps, StepsWithGoalSteps),
+	_solve(RemainingGoals, ParamsWithGoalParams, SolutionParams, StepsWithGoalSteps, SolutionSteps).
 
 %The question has already been answered
-solve(problem(Facts, totalTime), _, ProblemSteps, ProblemSteps, Facts) :-
-    _member(totalTime(TotalTime), Facts).
+_solve_subproblem(Goal, Params, Params, Steps, [Goal | Steps]) :-
+    _elaborate(Goal, GoalParam, _, _),
+    _member(GoalParam, Params).
 
-%Recursively answer the question by solving subproblems
-solve(problem(Facts, Question), CurrentSteps, [Answer | SolvedSteps], FinalFacts) :-
-    not _member(Question, CurrentSteps),
-    sub_questions(Question, SubQuestions),
-    solve(problem(Facts, SubQuestions), CurrentSteps, SolvedSteps, UpdatedFacts),
-	answer(Question, SubQuestions, Answer),
-	update(UpdatedFacts, Answer, FinalFacts).
+%Recursively answer the question by solving subquestions
+_solve_subproblem(Goal, Params, SolutionParams, Steps, [Goal | StepsWithSubgoalSteps]) :-
+    _not_revisited(Goal, Steps),
+    _elaborate(Goal, _, Subgoals, _),
+    _solve(Subgoals, Params, ParamsWithSubgoalParams, Steps, StepsWithSubgoalSteps),
+    _compute_solution(Goal, ParamsWithSubgoalParams, SolutionParams).
 
-sub_questions(totalTime, [v0(V0), theta(Theta), h0(H0), h(H), gravity(G), v0y(V0y)]).
+_not_revisited(Goal, Steps) :- not _member(Goal, Steps).
 
-answer(
+_compute_solution(Goal, Params, [Solution | Params]) :-
+    _elaborate(Goal, _, _, GoalParams),
+    _members(GoalParams, Params),
+    _resolve_goal(Goal, GoalParams, Solution).
+
+% Total Time
+_elaborate(
 totalTime,
-[v0(V0), theta(Theta), h0(H0), h(H), gravity(G), v0y(V0y)],
-totalTime(divide(sub(neg(V0y), sqrt(sub(pow(V0y, 2), multiply([2, G, sub(H0, H)])))), neg(G)))
+totalTime(TotalTime),
+[h0, hx, gravity, v0y],
+[h0(H0), hx(Hx), gravity(G), v0y(V0y)]
 ).
 
-update(Facts, NewFact, [NewFact|Facts]).
+_resolve_goal(
+totalTime,
+[h0(H0), hx(Hx), gravity(G), v0y(V0y)],
+totalTime(divide(sub(neg(V0y), sqrt(sub(pow(V0y, 2), multiply([2, G, sub(H0, Hx)])))), neg(G)))
+) :- totalTime(divide(sub(neg(V0y), sqrt(sub(pow(v0y, 2), multiply([2, G, sub(H0, Hx)])))), neg(G))).
+
+totalTime(X) :- not _not_totalTime(X).
+_not_totalTime(X) :- not totalTime(X).
+
+% V0Y
+_elaborate(
+v0y,
+v0y(V0y),
+[v0, theta],
+[v0(V0), theta(Theta)]
+).
+
+_resolve_goal(
+v0y,
+[v0(V0), theta(Theta)],
+v0y(multiply([V0,sin(Theta)]))
+) :- v0y(multiply([V0,sin(Theta)])).
+
+v0y(X) :- not _not_v0y(X).
+_not_v0y(X) :- not v0y(X).
+
+% h0
+_elaborate(
+h0,
+h0(H0),
+[],
+[]
+).
+
+% hx
+_elaborate(
+hx,
+hx(Hx),
+[],
+[]
+).
+
+% gravity
+_elaborate(
+gravity,
+gravity(G),
+[],
+[]
+).
+
+% theta
+_elaborate(
+theta,
+theta(Theta),
+[],
+[]
+).
+
+% v0
+_elaborate(
+v0,
+v0(V0),
+[],
+[]
+).
+
+%?- write('lala'), solve(problem([h0(1), h(2), gravity(9.8), theta(45), v0(100)], [totalTime(X)]), Steps).
+?- _solve([totalTime], [h0(1), hx(2), gravity(9.8), theta(45), v0(100)], Steps).
+
+
--- src/main/prolog/Utils.pl
+++ src/main/prolog/Utils.pl
@@ -1,5 +1,7 @@
-_debug([]) :- nl.
+_debug([]) :- nl. % it does not always do the println for some reason
 _debug([Arg|Arguments]):-
 	write(Arg), write(' '),
 	_debug(Arguments).
+
+
 
